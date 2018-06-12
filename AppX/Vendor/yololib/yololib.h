29ccc33086a33046fb18b83fdc41aa1b346425e5
Joey <qiaoxueshi@gmail.com>
2018-06-12 23:04:09 +0800

    Add GCDTask and yololib dependencies

---
+++ AppX/Vendor/yololib/yololib.h
@@ -0,0 +1,12 @@
+//
+//  yololib.h
+//  AppX
+//
+//  Created by Joey on 2018/6/12.
+//  Copyright © 2018 NoName. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+void inject_file(NSString* file, NSString* _dylib);
+
